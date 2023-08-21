618f50f3e18a3b6c0a35fca4897b148fa698931d
SamuelCastaAlf <92555846+SamuelCastaAlf@users.noreply.github.com>
2023-08-21 02:42:24 -0500

    Creación de las tres clases (Asiento, Auto y Motor)
    Se suben las tres clases requeridas en el ejercicio.

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,32 @@
+package test;
+public class Auto {
+	String modelo;
+	int precio;
+	Asiento[] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	int cantidadAsientos() {
+		int countAsientos = 0;
+		for(int i=0;i<asientos.length;i++) {
+			if(asientos[i]!=null) {
+				countAsientos++;
+			}
+			return countAsientos;
+		}
+		String verificarIntegridad() {
+			if(registro==motor.registro) {
+				for(int i=0;i<asientos.length;i++) {
+					if(asientos[i]!=null) {
+						if(asientos[i].registro!=registro) {
+							return "Las piezas no son originales";
+						}
+					}
+				}
+				return "Auto original";
+			}else {
+				return "Las piezas no son originales";
+		}
+	}
+}
